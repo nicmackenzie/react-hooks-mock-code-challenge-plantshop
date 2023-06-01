473103b6e3c6d1aa54bec01d5576b81f7fd485e9
nicmackenzie <nicmackenzie@live.com>
2023-06-01 12:19:16 +0300

    wrap App with AppProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import { AppProvider } from './context/app-context';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <AppProvider>
+    <App />
+  </AppProvider>,
+  document.getElementById('root')
+);
